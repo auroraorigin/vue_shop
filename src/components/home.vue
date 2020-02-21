b37fcb588cf663d8e548d92784a5eabee574ac13
auroraorigin <226608960@qq.com>
2020-02-22 01:44:24 +0800

    Revert "null"
    This reverts commit b983f27aba54526c9deb5d067f249d743a25b532.

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,129 +1,19 @@
 <template>
-  <el-container class="home-container">
-    <el-header>
-      <div>后台管理系统</div>
-      <el-button class="btn" type="info" @click="logout" round>退出</el-button>
-    </el-header>
-    <el-container>
-      <el-aside :width="isCollapse ? '64px':'200px'">
-        <div class="toggle-button" @click="toggleCollapse">|||</div>
-        <!-- 侧边菜单 -->
-        <el-menu
-          background-color="#293a4a"
-          text-color="#e9f3f4"
-          active-text-color="#409eff"
-          unique-opened
-          :collapse="isCollapse"
-          :collapse-transition="false"
-          :router="true"
-          :default-active="this.$route.path"
-        >
-          <!-- 一级菜单 -->
-          <el-submenu :index="item.id + ''" v-for="item in menulist" :key="item.id">
-            <!-- 一级菜单区域 -->
-            <template slot="title">
-              <!-- 图标 -->
-              <i :class="iconsObj[item.id]"></i>
-              <!-- 文本 -->
-              <span>{{item.authName}}</span>
-            </template>
-            <!-- 二级菜单 -->
-            <el-menu-item
-              :index="'/'+subItem.path"
-              v-for="subItem in item.children"
-              :key="subItem.id"
-            >
-              <template slot="title">
-                <!-- 图标 -->
-                <i class="el-icon-menu"></i>
-                <!-- 文本 -->
-                <span>{{subItem.authName}}</span>
-              </template>
-            </el-menu-item>
-          </el-submenu>
-        </el-menu>
-      </el-aside>
-      <el-main>
-        <router-view></router-view>
-      </el-main>
-    </el-container>
-  </el-container>
+  <div>
+    <el-button type="info" @click="logout">退出</el-button>
+  </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      menulist: [],
-      iconsObj: {
-        125: 'iconfont icon-user',
-        103: 'iconfont icon-tijikongjian',
-        101: 'iconfont icon-shangpin',
-        102: 'iconfont icon-danju',
-        145: 'iconfont icon-baobiao'
-      },
-      isCollapse: false
-    }
-  },
-  created() {
-    this.getMenuList()
-  },
   methods: {
     logout() {
       window.sessionStorage.clear()
       this.$router.push('/login')
-    },
-    async getMenuList() {
-      const { data: res } = await this.$http.get('menus')
-      if (res.meta.status !== 200) {
-        return this.$message.error(res.meta.msg)
-      }
-      this.menulist = res.data
-      console.log(res)
-    },
-    toggleCollapse() {
-      this.isCollapse = !this.isCollapse
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
-.home-container {
-  height: 100%;
-}
-.el-header {
-  background-color: rgb(32, 54, 75);
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  font-size: 25px;
-  color: #e9f3f4;
-  .btn {
-    background-color: rgb(32, 54, 75);
-  }
-}
-.el-aside {
-  background-color: #293a4a;
-  .el-menu {
-    border-right: none;
-  }
-}
-.el-main {
-  background-color: #e9edf1;
-}
-
-.iconfont {
-  margin-right: 10px;
-}
-
-.toggle-button {
-  background-color: #4a5064;
-  font-size: 10px;
-  line-height: 24px;
-  color: #e9edf1;
-  text-align: center;
-  letter-spacing: 0.2em;
-  cursor: pointer;
-}
 </style>
--- src/components/login.vue
+++ src/components/login.vue
@@ -78,6 +78,7 @@ export default {
           return this.$message.error('登录失败')
         }
         this.$message.success('登陆成功')
+        console.log(res)
         window.sessionStorage.setItem('token', res.data.token)
         this.$router.push('/home')
       })
